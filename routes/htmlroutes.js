c1fdc3cddb8a4edf6b30fdff5ed7a6292249e5df
rubenavaldez <rubenavaldez@yahoo.com>
2021-02-04 13:09:09 -0700

    added questions sorted by module

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -6,20 +6,39 @@ const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-acce
 module.exports = function(app){
   app.get('/test',(req,res)=>{ 
       console.log(req.query.test)
+    
      let testTitle = convertName(req.query.test)
-     console.log(req.query.test)
-     Test.find({name:req.query.test}).then(data=>console.log(data))
+     //console.log(req.query.test)
+     let timeLimit;
+     let questionAmount;
+     Test.find({name:req.query.test}).limit(1).exec((err,data)=>{
+       //console.log(data[0].timeLimit) 
+        if(data[0].timeLimit != undefined ){
+          timeLimit = parseInt(data[0].timeLimit)
+          console.log("database: " + timeLimit)          
+        }else{
+          timeLimit = 60
+        }
+
+        questionAmount= data[0].amount;
+        console.log("questions: " + questionAmount)
+     
 
 
-       Question.find({}).then(function(dbQuestions){
+       Question.find({module:req.query.test}).then(function(dbQuestions){
             //res.json({Question:dbQuestions})
            res.render('test',{
                Questions:dbQuestions.map(question => question.toJSON()),
-              testTitle:testTitle
+              testTitle:testTitle,
+              timeLimit:timeLimit,
+              questionAmount:questionAmount
               
               })
 
        })
+
+
+      })
         
     
     
